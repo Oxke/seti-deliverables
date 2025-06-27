32d81742ce0596eaa27c1c6769318777f4f4350d
Oxke <osea.fracchia@iusspavia.it>
2025-06-27 18:35:33 +0200

    add pictures and custom function

--- utils/osea_signal.py
+++ utils/osea_signal.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
 from matplotlib import pyplot as plt
-
+from astropy import units as u
 
 def osea_path(f_start, drift_rate, amplitude, omega, mu):
     def path(t):
-        return np.exp(-mu * t) * amplitude * np.sqrt(np.abs(np.sin(omega * t)))
-    return np.vectorize(lambda t: f_start + drift_rate * t + path(t))
+        return amplitude * np.sqrt(np.abs(np.sin(omega * t))) * np.exp(-mu * t)
+    return lambda t: f_start + drift_rate * t + path(t)
 
 if __name__ == "__main__":
     T = np.linspace(0, 300, 301)
