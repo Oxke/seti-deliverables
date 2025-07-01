570e45d8e2ca36d9448554b3f669cf8508ecdcef
Oxke <osea.fracchia@iusspavia.it>
2025-07-01 11:58:40 +0200

    change .py files locally

--- k218b_setigencore.py
+++ k218b_setigencore.py
@@ -1,12 +1,14 @@
-import os
 import glob
+import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from inject_signal import inject_signal
 
 DATA_FOLDER = "../../random-data/"
 OUTPUT_FOLDER = "../../k218b/"
 WINDOW_SIZE = 1024 # size in freq bins of injected signal
+SETICORE = "HDF5_PLUGIN_PATH=/home/obs/.conda/envs/seticore/lib/python3.12/site-packages/hdf5plugin/plugins seticore"
 
 os.makedirs(output_dir, exist_ok=True)
 
@@ -15,23 +17,28 @@ all_files = glob.glob(os.path.join(dir1, "k2-18b*/*0.fil"))
 
 # --- Define processing function for one file ---
 def process_file(filepath):
+    print(f"processing {filepath}")
     try:
+        print("  injecting signal")
         clean_path, injected_path = inject_signal(filepath, OUTPUT_FOLDER)
 
-        subprocess.run(["HDF5_PLUGIN_PATH seticore", clean_path, "--output", clean_path]check=True)
+        print("  running seticore")
+        subprocess.run([SETICORE, clean_path,
+                        "--output", out_clean := (clean_path[:-3] + ".dat")],
+                       check=True)
+        subprocess.run([SETICORE, clean_path,
+                        "--output", out_mod := (injected_path[:-3] + ".dat")],
+                       check=True)
 
-        with open(command_out2, "w") as fout2:
-            subprocess.run(["my_command", output2], stdout=fout2, check=True)
-
-        # Compute diff and count lines
-        diff_process = subprocess.run(["diff", command_out1, command_out2], capture_output=True, text=True)
+        print("  computing diff")
+        diff_process = subprocess.run(["diff", out_clean, out_mod], capture_output=True, text=True)
         diff_output = diff_process.stdout
         line_count = len(diff_output.strip().splitlines())
 
-        # Save diff
+        print("  saving diff")
         base_name = os.path.basename(filepath).replace(".fil", "")
-        diff_filename = f"{base_name}_diff_{line_count}_lines.txt"
-        diff_path = os.path.join(output_dir, diff_filename)
+        diff_filename = f"diff_{line_count}_lines_{base_name}.txt"
+        diff_path = os.path.join(OUTPUT_FOLDER, diff_filename)
         with open(diff_path, "w") as f:
             f.write(diff_output)
             
