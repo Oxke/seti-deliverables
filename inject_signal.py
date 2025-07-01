1a643136e5f59fd4bf41ca788f81137cefeec23a
Oxke <osea.fracchia@iusspavia.it>
2025-07-01 18:08:01 +0200

    add some examples found not found

--- inject_signal.py
+++ inject_signal.py
@@ -9,7 +9,7 @@ from os import path
 OUTPUT_FOLDER = "../../deliverables/injected/"
 WINDOW_SIZE = 1024 # size in freq bins of injected signal
 
-def make_frame_s(frame, min_freq_index, dr, snr, l, r):
+def make_frame_s(frame, min_freq_index, dr, snr, l, r, imgsave_path=None, output_folder=OUTPUT_FOLDER):
     frame_s = frame.get_slice(l,r)
     signal = frame_s.add_signal(
         stg.constant_path(f_start=frame_s.get_frequency(index=min_freq_index),
@@ -18,6 +18,9 @@ def make_frame_s(frame, min_freq_index, dr, snr, l, r):
         stg.gaussian_f_profile(width=100*u.Hz),
         stg.constant_bp_profile(level=1)
     )
+    if imgsave_path:
+        frame_s.plot()
+        plt.savefig(imgsave_path)
     return frame_s
 
 def inject_signal(fil_filepath, output_folder=OUTPUT_FOLDER, **kwargs):
@@ -29,9 +32,15 @@ def inject_signal(fil_filepath, output_folder=OUTPUT_FOLDER, **kwargs):
         l = np.random.choice(L)
 
     if "snr" in kwargs:
-        snr = kwargs["snr"]
+        if kwargs["snr"] is None:
+            SNR = 10 ** np.linspace(kwargs["min_snr"],
+                                    kwargs["max_snr"],
+                                    kwargs["n_snr"])
+            snr = np.random.choice(SNR)
+        else:
+            snr = kwargs["snr"]
     else:
-        SNR = 10 ** np.linspace(0, 3, 200)
+        SNR = 10 ** np.linspace(1, 3, 200)
         snr = np.random.choice(SNR)
 
     # I don't need it for now so I won't implement it, but if dr is passed then
@@ -52,10 +61,11 @@ def inject_signal(fil_filepath, output_folder=OUTPUT_FOLDER, **kwargs):
 
     frame.save_h5(cleanpath := output_folder + output_name + ".clean.h5")
 
-    frame.data[:, l:r] = make_frame_s(frame, mfi, dr, snr, l, r).data
+    image_path = output_folder + output_name + ".signal.png"
+    frame.data[:, l:r] = make_frame_s(frame, mfi, dr, snr, l, r, imgsave_path=image_path, output_folder=output_folder).data
     frame.save_h5(modpath := output_folder + output_name + ".mod.h5")
 
-    return cleanpath, modpath
+    return cleanpath, modpath, f"{mfi = }\n{dr = }\n{snr = }\nwindow = {l} - {r}"
 
 if __name__ == "__main__":
     filepath = "../../random-data/guppi_60703_16858_008036_TIC286923464_ON_0001.0000.fil"
