c89563b55684a66d4d47c1faa39b0952fdba031a
Oxke <osea.fracchia@iusspavia.it>
2025-07-03 11:01:34 +0200

    add exploretarget module

---
+++ exploretarget/gaia.py
@@ -0,0 +1,61 @@
+import numpy as np
+import astroquery.gaia as Gaia
+from astropy.coordinates import SkyCoord
+
+def query(query: str, *args, **kwargs):
+    """
+    Queries the gaia catalog, essentially just launches the job, catches the
+    results and returns them
+
+    Parameters:
+        query (str): query string to send to gaia
+    Returns:
+        results (astropy.table.table.Table): job results
+        job (astroquery.utils.tap.model.job.Job): job instance
+    """
+    job = Gaia.launch_job_async(query, *args, **kwargs)
+    results = job.get_results()
+
+    return results, job
+
+def circles(list_coords: list[SkyCoord], radius: float, *args, **kwargs):
+    """
+    Queries the gaia catalog for all the targets in a circle of center `coords`
+    and radius `radius`.
+
+    Parameters:
+        coords (list[astropy.coordinates.SkyCoord]): list of sky coordinates of
+            circle centers
+        radius (float): radius in degree or list of radiuses (length same as
+            list_coords
+    Returns:
+        results (astropy.table.table.Table): job results
+        job (astroquery.utils.tap.model.job.Job): job instance
+    """
+    if isinstance(radius, float): radius = [radius] * len(list_coords)
+    assert len(list_coords) == len(radius), "list_coords and radiuses have \
+        different dimentions"
+
+    centers = []
+    for coords in list_coords:
+        coords_icrs = coords.transform_to('icrs')
+        coords_icrs.location = None  # makes sure it's barycentric
+        coords_icrs.obstime = Time('J2016.0')
+        centers.append((coords_icrs.ra.deg, coords_icrs.dec.deg))
+
+    where_clause = " OR ".join([
+            f"CONTAINS(POINT('ICRS', ra, dec), CIRCLE('ICRS', {ra}, {dec},
+            {radius})) = 1"
+                for ra, dec in centers
+                    ])
+
+    query_str = f"""
+    SELECT *
+    FROM gaiadr3.gaia_source
+    WHERE CONTAINS(
+       POINT('ICRS', ra, dec),
+       CIRCLE('ICRS', {coords_icrs.ra.deg}, {coords_icrs.dec.deg}, {radius})
+    ) = 1
+    """
+
+    return query(query_str, *args, **kwargs)
---
+++ exploretarget/targets.py
@@ -0,0 +1,28 @@
+from targetexplore.gaia import circle
+
+class Targets:
+    """
+    A class to represent a person.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+    first name of the person
+    surname : str
+    family name of the person
+    age : int
+        age of the person
+
+    Methods
+    -------
+    info(additional=""): Prints the person's name and age.
+    """
+    def __init__(self, centers, radius):
+        """
+        Initialize a Targets object
+
+        self.centers = centers
+        self.
+
